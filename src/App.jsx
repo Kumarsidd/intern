776e6eb8b0aad008d735ea69d08be3e7278f14e7
kumarsidd <kumarsiddhesh159@gmail.com>
2023-05-18 20:43:57 +0530

    page alignemnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
-import Arrival from "./pages/Arrival";
 
 const App = () => {
   return (
     <>
       <Home />
-      <Arrival />
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import "./index.css";
 import Navbar from "../components/navbar/Navbar";
 import Landing from "../components/landing/Landing";
+import Column from "../components/column/Column";
+import Seller from "../components/seller/Seller";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
-    <section>
-      <Navbar />
-      <Landing />
-    </section>
+    <>
+      <section>
+        <Navbar />
+        <Landing />
+      </section>
+      <Column />
+      <Seller />
+      <Footer />
+    </>
   );
 };
 
