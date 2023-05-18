04bd75ac4c0031d6fe0a55eac966c1dbba05c882
kumarsidd <kumarsiddhesh159@gmail.com>
2023-05-18 13:50:16 +0530

    landing

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./landing.css";
+const Landing = () => {
+  return (
+    <>
+      <main className="landing">
+        <div className="content">
+          <h1>EXPERIENCE THE POWER WITHIN</h1>
+          <p>
+            Claritas est etiam processus dynamicus, qui sequitur mutationem
+            consuetudium
+            <br /> lectorum. Mirum est notare quam littera gothica, quam nunc
+            putamus parum claram.
+          </p>
+          <button>
+            VIEW COLLECTION <i className="arrow right"></i>
+          </button>
+        </div>
+      </main>
+      <main></main>
+    </>
+  );
+};
+
+export default Landing;
