41b5094807271d608c65452381786ca2cc33459b
kumarsidd <kumarsiddhesh159@gmail.com>
2023-05-18 17:26:41 +0530

    seller

--- src/pages/Arrival.jsx
+++ src/pages/Arrival.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Column from "../components/column/Column";
+import Seller from "../components/seller/Seller";
 const Arrival = () => {
   return (
     <>
       <Column />
+      <Seller />
     </>
   );
 };
